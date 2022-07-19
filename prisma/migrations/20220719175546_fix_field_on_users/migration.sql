1508c65cd38d06b78293eb06d93203ac27a172de
Shedy <shedyhs@gmail.com>
2022-07-19 13:57:31 -0400

    🗃️ Add migration to fix typo on last migration

---
+++ prisma/migrations/20220719175546_fix_field_on_users/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "ddi" TEXT NOT NULL,
+    "ddd" TEXT NOT NULL,
+    "number" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
