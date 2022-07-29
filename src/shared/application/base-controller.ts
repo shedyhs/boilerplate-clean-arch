b9e13d92b3561bbb4102b3c90f2f7d46158c1561
Shedy <shedyhs@gmail.com>
2022-07-29 15:48:22 -0400

    🎨 Add enum to have http status codes

--- src/shared/application/base-controller.ts
+++ src/shared/application/base-controller.ts
@@ -4,6 +4,7 @@ import { DomainError } from '@/shared/domain/domain-error';
 import { HttpRequest, HttpResponse } from '@/shared/interfaces/http';
 import { IValidator } from './validations/validator-interface';
 import { ServerErrors } from './server-error';
+import { HttpStatusCode } from '../interfaces/http-status-codes';
 
 export abstract class BaseController {
   abstract perform(request: HttpRequest): Promise<HttpResponse>;
@@ -23,96 +24,211 @@ export abstract class BaseController {
       const err = error as Error;
       switch (err.constructor) {
         case DomainError:
-          return { data: { error: err.message }, statusCode: 400 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.BadRequest,
+          };
         case ApplicationErrors.UnauthorizedError:
-          return { data: { error: err.message }, statusCode: 401 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.Unauthorized,
+          };
         case ApplicationErrors.PaymentRequired:
-          return { data: { error: err.message }, statusCode: 402 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.PaymentRequired,
+          };
         case ApplicationErrors.ForbiddenError:
-          return { data: { error: err.message }, statusCode: 403 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.Forbidden,
+          };
         case ApplicationErrors.NotFoundError:
-          return { data: { error: err.message }, statusCode: 404 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.NotFound,
+          };
         case ApplicationErrors.MethodNotAllowed:
-          return { data: { error: err.message }, statusCode: 405 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.MethodNotAllowed,
+          };
         case ApplicationErrors.NotAcceptable:
-          return { data: { error: err.message }, statusCode: 406 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.NotAcceptable,
+          };
         case ApplicationErrors.ProxyAuthenticationRequired:
-          return { data: { error: err.message }, statusCode: 407 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.ProxyAuthenticationRequired,
+          };
         case ApplicationErrors.RequestTimeout:
-          return { data: { error: err.message }, statusCode: 408 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.RequestTimeout,
+          };
         case ApplicationErrors.ConflictError:
-          return { data: { error: err.message }, statusCode: 409 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.Conflict,
+          };
         case ApplicationErrors.Gone:
-          return { data: { error: err.message }, statusCode: 410 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.Gone,
+          };
         case ApplicationErrors.LengthRequired:
-          return { data: { error: err.message }, statusCode: 411 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.LengthRequired,
+          };
         case ApplicationErrors.PreconditionFailed:
-          return { data: { error: err.message }, statusCode: 412 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.PreconditionFailed,
+          };
         case ApplicationErrors.PayloadTooLarge:
-          return { data: { error: err.message }, statusCode: 413 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.PayloadTooLarge,
+          };
         case ApplicationErrors.RequestURITooLarge:
-          return { data: { error: err.message }, statusCode: 414 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.RequestURITooLarge,
+          };
         case ApplicationErrors.UnsupportedMediaType:
-          return { data: { error: err.message }, statusCode: 415 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.UnsupportedMediaType,
+          };
         case ApplicationErrors.RequestRangeNotSatisfiable:
-          return { data: { error: err.message }, statusCode: 416 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.RequestRangeNotSatisfiable,
+          };
         case ApplicationErrors.ExpectationFailed:
-          return { data: { error: err.message }, statusCode: 417 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.ExpectationFailed,
+          };
         case ApplicationErrors.ImATeapot:
-          return { data: { error: err.message }, statusCode: 418 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.ImATeapot,
+          };
         case ApplicationErrors.MisdirectedRequest:
-          return { data: { error: err.message }, statusCode: 421 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.MisdirectedRequest,
+          };
         case ApplicationErrors.UnprocessableEntity:
-          return { data: { error: err.message }, statusCode: 422 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.UnprocessableEntity,
+          };
         case ApplicationErrors.Locked:
-          return { data: { error: err.message }, statusCode: 423 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.Locked,
+          };
         case ApplicationErrors.FailedDependency:
-          return { data: { error: err.message }, statusCode: 424 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.FailedDependency,
+          };
         case ApplicationErrors.UpgradeRequired:
-          return { data: { error: err.message }, statusCode: 426 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.UpgradeRequired,
+          };
         case ApplicationErrors.PreconditionRequired:
-          return { data: { error: err.message }, statusCode: 428 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.PreconditionRequired,
+          };
         case ApplicationErrors.TooManyRequests:
-          return { data: { error: err.message }, statusCode: 429 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.TooManyRequests,
+          };
         case ApplicationErrors.RequestHeaderFieldsTooLarge:
-          return { data: { error: err.message }, statusCode: 431 };
-        case ApplicationErrors.ConnectionClosedWithoutResponse:
-          return { data: { error: err.message }, statusCode: 444 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.RequestHeaderFieldsTooLarge,
+          };
         case ApplicationErrors.UnavailableForLegalReasons:
-          return { data: { error: err.message }, statusCode: 451 };
-        case ApplicationErrors.ClientClosedRequest:
-          return { data: { error: err.message }, statusCode: 499 };
+          return {
+            data: { error: err.message },
+            statusCode: HttpStatusCode.UnavailableForLegalReasons,
+          };
 
         case ServerErrors.NotImplemented:
-          return { data: { error: 'Internal Server Error' }, statusCode: 501 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.InternalServerError,
+          };
         case ServerErrors.BadProvider:
-          return { data: { error: 'Internal Server Error' }, statusCode: 502 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.BadProvider,
+          };
         case ServerErrors.ServiceUnavailable:
-          return { data: { error: 'Internal Server Error' }, statusCode: 503 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.ServiceUnavailable,
+          };
         case ServerErrors.ProviderTimeout:
-          return { data: { error: 'Internal Server Error' }, statusCode: 504 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.ProviderTimeout,
+          };
         case ServerErrors.HTTPVersionNotSupported:
-          return { data: { error: 'Internal Server Error' }, statusCode: 505 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.HttpVersionNotSupported,
+          };
         case ServerErrors.VariantAlsoNegotiates:
-          return { data: { error: 'Internal Server Error' }, statusCode: 506 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.VariantAlsoNegotiates,
+          };
         case ServerErrors.InsufficientStorage:
-          return { data: { error: 'Internal Server Error' }, statusCode: 507 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.InsufficientStorage,
+          };
         case ServerErrors.LoopDetected:
-          return { data: { error: 'Internal Server Error' }, statusCode: 508 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.LoopDetected,
+          };
         case ServerErrors.NotExtended:
-          return { data: { error: 'Internal Server Error' }, statusCode: 510 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.NotExtended,
+          };
         case ServerErrors.NetworkAuthenticationRequired:
-          return { data: { error: 'Internal Server Error' }, statusCode: 511 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.NetworkAuthenticationRequired,
+          };
         case ServerErrors.NetworkConnectionTimeoutError:
-          return { data: { error: 'Internal Server Error' }, statusCode: 599 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.NetworkConnectionTimeoutError,
+          };
         default:
           // eslint-disable-next-line no-console
           console.error(err);
-          return { data: { error: 'Internal Server Error' }, statusCode: 500 };
+          return {
+            data: { error: 'Internal Server Error' },
+            statusCode: HttpStatusCode.InternalServerError,
+          };
       }
     }
   }
-
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   buildValidators(request: HttpRequest): IValidator[] {
     return [];
